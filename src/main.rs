61926709ee673def12eeec398e88e9f2794c957b
Daniel Cumbor <dcumbor@ik.me>
2025-05-04 15:42:54 +0100

    Formatted Code.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ fn fizz_buzz(current_number: u32) -> String {
     let mut rules = BTreeMap::new();
     rules.insert(3, "Fizz");
     rules.insert(5, "Buzz");
-    rules.insert(7, "Bazz");
 
-    let result: String = rules.iter().filter_map(|(&divisor, &word)| {
-        if current_number % divisor == 0 {
-            Some(word)
-        } else {
-            None
-        }
-    }).collect();
+    let result: String = rules
+        .iter()
+        .filter_map(|(&divisor, &word)| {
+            if current_number % divisor == 0 {
+                Some(word)
+            } else {
+                None
+            }
+        })
+        .collect();
 
     if result.is_empty() {
         current_number.to_string()
